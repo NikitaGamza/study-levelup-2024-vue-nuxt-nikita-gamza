575c8c1441c071cabb13c0bbb01ee35db9e4e9ff
Gamza <gamza-ns@mail.ru>
2024-04-23 08:14:53 +0300

    init: initial project

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'levelup-vue-nuxt',
+      script: 'serve',
+      watch: 'dist',
+      instances: '1',
+      exec_mode: 'fork',
+      env: {
+        PM2_SERVE_PATH: 'dist',
+        PM2_SERVE_PORT: 8080,
+        PM2_SERVE_SPA: 'true',
+      },
+    },
+  ],
+};
